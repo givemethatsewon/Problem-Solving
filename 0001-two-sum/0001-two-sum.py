588b24b8b9959d8103868224d449d8092922678a
Sewon Min <86864525+givemethatsewon@users.noreply.github.com>
2024-03-02 12:36:18 +0900

    Time: 60 ms (64.38%), Space: 17.8 MB (59.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,25 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        table = {}
+        n = len(nums)
+
+        #해시 테이블 만들기 
+        for i in range(n):
+            table[nums[i]] = i
+
+        for i in range(n):
+            pair = target - nums[i]
+            if pair in table and i != table[pair]:
+                return [i, table[pair]]
+        
+
+
+
+
+        
+        
+
+        
 
 
 
